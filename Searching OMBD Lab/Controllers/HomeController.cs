b712d31a793002d05083f6a37f8f9c7a5e32f648
carissa-c <122126939+carissa-c@users.noreply.github.com>
2023-02-14 22:51:43 -0600

    upload

---
+++ Searching OMBD Lab/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Searching_OMBD_Lab.Models;
+
+namespace Searching_OMBD_Lab.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    public IActionResult MovieSearchForm()
+    {
+        return View("MovieSearch");
+    }
+    [HttpPost]
+    public IActionResult MovieSearchResults(string Title)
+    {
+        Movies result = MoviesDAL.GetMovie(Title);
+        return View(result);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ Searching OMBD Lab/Models/MoviesDAL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Searching_OMBD_Lab.Models
+{
+	public class MoviesDAL
+	{
+        public static Movies GetMovie(string input)//adjust here
+        {
+            //Adjust here
+            //Setup
+            string url = $"https://www.omdbapi.com/?t={input}&apikey={Secret.key}";
+
+            //request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Convert it to JSON
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+
+            //Adjust here
+            //Convert to C#
+            Movies result = JsonConvert.DeserializeObject<Movies> (JSON);
+            return result;
+        }
+    }
+}
+
